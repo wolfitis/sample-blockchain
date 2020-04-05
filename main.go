0c13c93f0e15aa4f65f42ad23c59194910216a53
wolfitis <webblog123@gmail.com>
2020-04-05 11:58:52 -0400

    updated after cli functionality

--- main.go
+++ main.go
@@ -1,22 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
 	bc := NewBlockchain()
-	bc.AddBlock("Send 1 BTC to user1")
-	bc.AddBlock("Send 3 more BTC to user1")
+	defer bc.db.Close()
 
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
