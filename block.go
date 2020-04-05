0cf7895180c6a75131f13c08771482bad760be8a
wolfitis <webblog123@gmail.com>
2020-04-05 07:37:55 -0400

    proof-of-work, able to perform mining

--- block.go
+++ block.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
 )
 
@@ -13,21 +10,27 @@ type Block struct {
 	Data          []byte // actual information
 	PrevBlockHash []byte // hash of previous block
 	Hash          []byte // hash of block
+	Nonce         int
 }
 
 // SetHash - temporary fucntion to calculate hashes
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
+// func (b *Block) SetHash() {
+// 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+// 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+// 	hash := sha256.Sum256(headers)
 
-	b.Hash = hash[:]
-}
+// 	b.Hash = hash[:]
+// }
 
 // NewBlock - simplify the creation of a block
 func NewBlock(data string, PrevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
--- proofofwork.go
+++ proofofwork.go
@@ -70,3 +70,16 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 
 	return nonce, hash[:]
 }
+
+// Validate - to validate proof-of-work
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+
+	return isValid
+}
